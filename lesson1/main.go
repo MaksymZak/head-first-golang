590dcf411dfc98b134cb89ccea36fcb2a66fa496
Zak <zaksumy1989@gmail.com>
2024-12-03 00:40:36 +0200

    just commit

---
+++ lesson1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt" // для вывода в консоль используется пакет fmt
+	"math"
+	"strings"
+)
+
+// для работы со строками используется пакет strings
+
+func main() {
+	fmt.Println("Hello", 100, 200, 300, "aadwawd")
+	fmt.Println(math.Ceil(2.2))
+	fmt.Println(strings.Title("hello world"))
+
+}
