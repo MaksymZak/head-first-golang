e4ce42441f1e3a704362f0e809a1619ffc0f2f38
Zak <zaksumy1989@gmail.com>
2025-01-02 20:01:16 +0200

    finish 109 pages

--- create_game/game.go
+++ create_game/game.go
@@ -1,3 +1,5 @@
+// guess - A simple number guessing game
+
 package main
 
 import (
@@ -12,34 +14,43 @@ import (
 )
 
 func main() {
-
+	count := 10
 	rand.NewSource(time.Now().Unix())
 	target := rand.Intn(100) + 1
 	fmt.Print("Chose a number between 1 and 100: ")
 
 	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
 
-	if err != nil {
-		log.Fatal(err)
+	success := false
+
+	for guesses := 0; guesses < count; guesses++ {
+		fmt.Println("You have", count-guesses, "guesses left")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
 
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-
-	if err != nil {
-		log.Fatal(err)
+	if !success {
+		fmt.Println("You lose. The correct number was", target)
 	}
 
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH")
-	} else {
-		fmt.Println("Your guess is correct")
-	}
-
-	for i := 2; i <= 3; i++ {
-		fmt.Println("You have", i, "guesses left")
-	}
 }
